d96d0a8f7b530504af5fd905be07ced1a30dee61
DAAN <daanmgneni@gmail.com>
2023-03-09 10:46:20 +0530

    Added Display Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+}
