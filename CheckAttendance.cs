8cc4c20beaecf57e8e2e23f349d6458d40801fd4
DAAN <daanmgneni@gmail.com>
2023-03-09 10:54:08 +0530

    Added program to check employee attendance

---
+++ CheckAttendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class CheckAttendance
+    {
+        public static void Attendance()
+        {
+            Random random = new Random();
+            int IsEmployeePresent=random.Next(2);
+            if (IsEmployeePresent == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            
+
+        }
+    }
+}
